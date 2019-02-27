b20152fb2e8dd06bfaf7a1bb1ae67c804484a095
Rai Butera <rai@rbutera.com>
2019-02-27 16:36:41 +0000

    removed z-index

--- .next/static/chunks/styles.js
+++ .next/static/chunks/styles.js
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1551284959901");
+            injectCss(link, link.href.split("?")[0] + "?unix=1551285395822");
           });
       }
     }
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "39f93eb1291848be46c6";
+/******/ 	var hotCurrentHash = "f85016c99cce20fab64c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
