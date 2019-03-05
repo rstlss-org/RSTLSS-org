bffbaa40a50f039bd972eacc82b3286e8509d5a2
Rai Butera <rai@rbutera.com>
2019-03-05 14:00:21 +0000

    improved navi mobile ux

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c1a189df009ee92c35f0";
+/******/ 	var hotCurrentHash = "a6d1f07c9965532ddf21";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
